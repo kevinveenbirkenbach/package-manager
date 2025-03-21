b68e5857e9e4ed8c8f9373a38c161257901e9fb3
Kevin Veen-Birkenbach <kevin@veen.world>
2025-03-21 18:09:19 +0100

    Implemented --break-system-packages for pip install

--- pkgmgr/install_repos.py
+++ pkgmgr/install_repos.py
@@ -108,7 +108,7 @@ def install_repos(selected_repos, repositories_base_dir, bin_dir, all_repos, no_
         req_txt_file = os.path.join(repo_dir, "requirements.txt")
         if os.path.exists(req_txt_file):
             print(f"requirements.txt found in {repo_identifier}, installing Python dependencies...")
-            cmd = "python3 -m pip install -r requirements.txt"
+            cmd = "python3 -m pip install -r requirements.txt --break-system-packages"
             run_command(cmd, cwd=repo_dir, preview=preview)
         
         # Check if a Makefile exists and run make.
