c5c84704db2a6aca11acba7b60d5c27edf62d43d
Kevin Veen-Birkenbach <kevin@veen.world>
2025-12-17 22:15:48 +0100

    Release version 1.8.5

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
         rec {
           pkgmgr = pyPkgs.buildPythonApplication {
             pname   = "package-manager";
-            version = "1.9.0";
+            version = "1.8.5";
 
             # Use the git repo as source
             src = ./.;
